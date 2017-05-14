efd039aeca71da312accb79d4965af8ecb571978
Falko Hofmann <falkohofmann@gmail.com>
2017-05-14 21:15:33 +0200

    bug fixing

--- estimate_motif_significance.R
+++ estimate_motif_significance.R
@@ -1,4 +1,4 @@
-options(echo=TRUE)
+ptions(echo=TRUE)
 args <- commandArgs(trailingOnly = TRUE)
 print(args)
 
@@ -7,6 +7,7 @@ test.values.file =  args[1]
 random.values.file = args[2]
 output.file =  args[3]
 output.plot = args[4]
+sample.name = args[5]
 
 #read in line count from the test sample
 test.value = try(system(paste('wc -l', test.values.file) ,intern = TRUE))
@@ -15,12 +16,16 @@ test.value <- sapply(strsplit(test.value, " "), "[", 3)
 #read in data
 random.dist.values <- read.table(random.values.file, header=F, sep =' ', stringsAsFactors = F)
 #fit and estimate ecdf
-P = ecdf(random.dist.values$V1)    
+P = ecdf(random.dist.values$V1)
 
 #get probabilty value for the "measured value" 
 test.prob <- P(test.value)
 #calcualte pvalue
+
 p.value <- 1-test.prob
+
+results.df <- data.frame(sample=sample.name, p_value=p.value)
+write.table(results.df, file = output.file, col.names = F, row.names = F, sep = '\t', quote = F)
 
 #plot ecdf with the p-value
 png(output.plot)
