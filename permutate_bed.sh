1ada3c830041c5d55ab2bfc984ec8d26efca969c
falko.hofmann <falkohofmann@gmail.com>
2017-05-14 18:41:05 +0000

    fixed some bugs

--- permutate_bed.sh
+++ permutate_bed.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #PBS -P rnaseq_nod
 #PBS -N motif_enrichment
-#PBS -J 1-2
+#PBS -J 1-1000
 #PBS -j oe
 #PBS -q workq
-#PBS -o /lustre/scratch/users/$USER/tfbs-matching/log/shuffeling/shuffeling_^array_index^.log
-#PBS -l walltime=0:30:00
+#PBS -o /lustre/scratch/users/falko.hofmann/pipelines/tfbs-analysis-utils/log/shuffeling/shuffeling_^array_index^.log
+#PBS -l walltime=0:10:00
 #PBS -l select=1:ncpus=4:mem=8gb
 
 #set variables
@@ -16,18 +16,18 @@ base_dir=/lustre/scratch/users/$USER/tfbs-matching
 script_dir=/lustre/scratch/users/$USER/pipelines/tfbs-analysis-utils
 
 #to parallelize and make shuffeling reproducible
-pbs_mapping_file=$script_dir/utils/seeds.txt
+pbs_mapping_file=$script_dir/utils/seed.txt
 
-bed_file=$granges_files/open_chrom_embryo.bed
+bed_file=/lustre/scratch/users/$USER/peak_calling/fseq/merges/embryo_specific.bed
 
-shuffled_regions=$granges_files/shuffled
+shuffled_regions=$base_dir/shuffled_bed
 
 mkdir $test_region_motifs
 mkdir $shuffled_regions
 
 #file for shuffeling
-tair10_nuclear=$script_dir/utils/tair10_size.txt
-tair10_exclude=$script_dir/utils/tair10_exclude.txt
+tair10_nuclear=$script_dir/utils/tair10.genome
+tair10_exclude=$script_dir/utils/tair10_blacklist.txt
 
 ## build array index
 ##### Obtain Parameters from mapping file using $PBS_ARRAY_INDEX as line number
@@ -44,12 +44,6 @@ echo 'Shuffeling file: '$bed_file
 echo 'Seed: ' $seed
 echo '#########################################################################'
 
-bedtools shuffle -i $granges_file \
--g $tair10_nuclear \
--excl $tair10_exclude \
--chrom \
--noOverlapping \
--seed $seed
-> $shuffled_regions/$seed.bed
+eval "bedtools shuffle -i $bed_file -g $tair10_nuclear -excl $tair10_exclude -chrom -noOverlapping -seed $seed > $shuffled_regions/$seed.bed"
 
 echo 'Finished shuffeling mapping for: '$seed
